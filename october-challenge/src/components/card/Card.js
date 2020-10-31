bbd886ab690f3eff2733c0599fef5e33cb9c99af
tote93 <igorej@hotmail.com>
2020-10-31 11:13:24 +0100

    Some fix

--- october-challenge/src/components/card/Card.js
+++ october-challenge/src/components/card/Card.js
@@ -1,23 +1,29 @@
-import React from 'react'
-import '../styles/Card.css'
-import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
-import ArrowDropUpIcon from '@material-ui/icons/ArrowDropUp';
-import {useStateValue} from '../../StateProvider'
-function Card({Icon, userName, count, increase, type}) {
-    const [{darkMode}] = useStateValue();
-    return (
-        <div className="card">
-        <p className="card__userName">{Icon}@{userName}</p>
-        <div className="card__main">
-            <h2>{count}
-            </h2>
-            <h3>followers</h3>        
-        </div>
-        <p className={`card__increase ${type && "benefits"} ${darkMode && "invert" }`}>
-        {type ? <ArrowDropUpIcon/> : <ArrowDropDownIcon />}
-        {increase} today</p>
+import React from "react";
+import "../styles/Card.css";
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
+import ArrowDropUpIcon from "@material-ui/icons/ArrowDropUp";
+import { useStateValue } from "../../StateProvider";
+function Card({ Icon, userName, count, increase, type }) {
+  const [{ darkMode }] = useStateValue();
+  return (
+    <div className="card">
+      <p className="card__userName">
+        {Icon}@{userName}
+      </p>
+      <div className="card__main">
+        <h2>{count}</h2>
+        <h3>followers</h3>
       </div>
-    )
+      <p
+        className={`card__increase ${type && "benefits"} ${
+          darkMode && "invert"
+        }`}
+      >
+        {type ? <ArrowDropUpIcon /> : <ArrowDropDownIcon />}
+        {increase} today
+      </p>
+    </div>
+  );
 }
 
-export default Card
+export default Card;
